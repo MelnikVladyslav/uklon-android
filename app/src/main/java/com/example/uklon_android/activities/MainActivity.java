72eb050d17e568852014a99be833375c7edf0eaf
MelnikVladyslav <115177420+MelnikVladyslav@users.noreply.github.com>
2023-06-29 16:14:14 +0300

    Add connect with server and test get

--- app/src/main/java/com/example/uklon_android/activities/MainActivity.java
+++ app/src/main/java/com/example/uklon_android/activities/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.uklon_android.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.uklon_android.R;
+import com.example.uklon_android.classes.Transport;
+import com.example.uklon_android.interfaces.ApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://localhost:7184")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(ApiService.class);
+
+        Call<List<Transport>> call = apiService.getDataList();
+        call.enqueue(new Callback<List<Transport>>() {
+            @Override
+            public void onResponse(Call<List<Transport>> call, Response<List<Transport>> response) {
+                if (response.isSuccessful()) {
+                    List<Transport> dataList = response.body();
+                    TextView textView = findViewById(R.id.textView);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (Transport data : dataList) {
+                        stringBuilder.append(data.getId()).append(": ").append(data.getModel()).append("\n");
+                    }
+                    textView.setText(stringBuilder.toString());
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Помилка обробки", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Transport>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Помилка: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
