0cff1078bef5493bea6f490c865cf6bde3b3422d
MelnikVladyslav <115177420+MelnikVladyslav@users.noreply.github.com>
2023-07-01 11:53:17 +0300

    Update

---
+++ app/src/main/java/com/example/uklon_android/activities/PhoneCodeActivity.java
@@ -0,0 +1,16 @@
+package com.example.uklon_android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uklon_android.R;
+
+public class PhoneCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_code);
+    }
+}
