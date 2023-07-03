26075c6bad2e23c4f1e476b4b645f3c7757bdadc
MelnikVladyslav <115177420+MelnikVladyslav@users.noreply.github.com>
2023-07-03 12:44:41 +0300

    Update

--- app/src/main/java/com/example/uklon_android/interfaces/ApiService.java
+++ app/src/main/java/com/example/uklon_android/interfaces/ApiService.java
@@ -5,9 +5,15 @@ import com.example.uklon_android.classes.Transport;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public interface ApiService {
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:5289")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
     @GET("/api/transports")
     Call<List<Transport>> getDataList();
 }
