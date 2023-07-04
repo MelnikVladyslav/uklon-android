a9d050513243685df089ea81e115d71d39d2917c
MelnikVladyslav <115177420+MelnikVladyslav@users.noreply.github.com>
2023-07-04 11:00:13 +0300

    Connect basic design

--- app/src/main/java/com/example/uklon_android/activities/PhoneCodeActivity.java
+++ app/src/main/java/com/example/uklon_android/activities/PhoneCodeActivity.java
@@ -1,16 +1,61 @@
 package com.example.uklon_android.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uklon_android.R;
 
 public class PhoneCodeActivity extends AppCompatActivity {
 
+    private EditText codeEditText;
+    private Button loginButton;
+    public String codeGen;
+
+    private boolean isValidCode(String code)
+    {
+        if (code == codeGen)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_code);
+
+        codeEditText = findViewById(R.id.editTextCode);
+        loginButton = findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Виконати логіку для перевірки номера телефону та входу
+                // наприклад, відправити OTP-повідомлення на номер телефону та перехід до наступної активності
+
+                // Приклад перехіду до наступної активності після успішного входу
+
+                String codeEntred = codeEditText.getText().toString();
+
+                if (isValidCode(codeEntred)) {
+                    Intent intent = new Intent(PhoneCodeActivity.this, EnterNamesActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(PhoneCodeActivity.this, "Невірний код", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/uklon_android/activities/StartActivity.java
@@ -0,0 +1,32 @@
+package com.example.uklon_android.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uklon_android.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+        startButton = findViewById(R.id.button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+              startActivity(intent);
+              finish();
+            }
+        });
+    }
+}
