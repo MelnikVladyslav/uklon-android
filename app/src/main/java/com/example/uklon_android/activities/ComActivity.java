215f8f31050cd3ca6ac93bebedef430c9b329e56
trafikjunior <anton05and@gmail.com>
2023-08-19 02:57:38 +0300

    added all pages for order taxi, delivery, driver, intercity

--- app/src/main/java/com/example/uklon_android/activities/ComActivity.java
+++ app/src/main/java/com/example/uklon_android/activities/ComActivity.java
@@ -11,7 +11,7 @@ public class ComActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.comment_driver);
+        setContentView(R.layout.comment);
 
     }
 }
